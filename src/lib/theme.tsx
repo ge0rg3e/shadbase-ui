98bfd171ffb3a6f0eb7d43c2c67c99b0aadd3402
Ge0rg3e <georgedeliu2006@gmail.com>
2025-07-04 17:51:54 +0300

    fix

--- src/lib/theme.tsx
+++ src/lib/theme.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 const useTheme = () => {
-	const [theme, setTheme] = useState<'light' | 'dark'>('light');
+	const [theme, setTheme] = useState<'light' | 'dark'>('dark');
 
 	const changeTheme = (theme: 'light' | 'dark') => {
 		setTheme(theme);
